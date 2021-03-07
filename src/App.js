fb0ce6a7d8b21e35aeef2d0b11e2d997366ca6ba
Kat Leight <leightkt@gmail.com>
2021-03-07 12:45:32 -0700

    connected bluetooth and turn on and off with button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
+import Bluetooth from './Components/Bluetooth'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        device: "",
+        characteristic: "",
+        server: "",
+        service: "",
+        commands: []
+    }
+
+    setDevice = (device) => {
+        this.setState({ device })
+    }
+
+    setCharacteristic = (characteristic) => {
+        this.setState({ characteristic })
+    }
+
+    setServer = (server) => {
+        this.setState({ server })
+    }
+
+    setService = (service) => {
+        this.setState({ service })
+    }
+    
+    turnOn = () => {
+        this.state.characteristic.writeValue(
+            new Uint8Array([0x0F, 0x03, 0x00, 0x07, 0x07, 0x03, 0x00, 0x00])
+            ).then(console.log)
+    }
+
+    turnOff = () => {
+        this.state.characteristic.writeValue(
+            new Uint8Array([0x1E, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+            ).then(console.log)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Voice2Vibes</h1>
+                    <Bluetooth 
+                        setDevice={ this.setDevice }
+                        setCharacteristic={ this.setCharacteristic }
+                        setServer={ this.setServer }
+                        setService={ this.setService }/>
+                    <button onClick={ this.turnOn }>ON</button>
+                    <button onClick={ this.turnOff }>OFF</button>
+                </header>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/Components/Bluetooth.js
@@ -0,0 +1,31 @@
+
+function Bluetooth({ setDevice, setCharacteristic, setServer, setService }) {
+
+    const connectToDevice = async () => {
+        const device = await navigator.bluetooth
+        .requestDevice({
+            filters: [
+                { namePrefix: "Moxie" },
+                { services: [ 'f000bb03-0451-4000-b000-000000000000' ]}
+            ]
+        })
+        setDevice(device)
+        const server = await device.gatt.connect()
+        setServer(server)
+        const service = await server.getPrimaryService('f000bb03-0451-4000-b000-000000000000')
+        setService(service)
+        const characteristic = await service.getCharacteristic('f000c000-0451-4000-b000-000000000000')
+        setCharacteristic(characteristic)
+    }
+
+    
+
+    return (
+        <>
+        <button onClick={connectToDevice}>CONNECT</button>
+        </>
+    )
+
+}
+
+export default Bluetooth
