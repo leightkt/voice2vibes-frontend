6ed87719deb3c679fd79a88d0e588dd21b7ca8ca
Kat Leight <leightkt@gmail.com>
2021-03-10 18:49:48 -0700

    add about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,39 @@ const About = (props) => {
 
     return(
         <section className="about-section">
-            <h2>About This App</h2>
+            <h2 className="about-title">LEVEL UP YOUR VIBRATOR</h2>
+            <div className="about-div">
+                <p className="about-text">
+                <h4>ABOUT</h4>
+                This App is built for 
+                the <a href="https://www.we-vibe.com/us/moxie">Moxie</a> by <a href="https://www.we-vibe.com/us/">We-Vibe</a>. 
+                The Moxie is a bluetooth connectable wearable vibrator for people with vulvas. 
+                We-Vibe makes an app for your phone that connects to the vibrator and allows
+                a user to interact with the vibrator via touch settings. You can also connect to a partner remotely
+                and allow them to interact with the vibrator over WiFi.
+                </p>
+                <p className="about-text">
+                <h4>CONNECT</h4>
+                This app uses the Web Bluetooth API technology to connect to your vibrator 
+                from your browser and the Web Speech API
+                to allow you to send commands via your voice.
+                When you log in (or sign up) click the "connect" button
+                which will open up a dialogue box. Make sure your vibrator is on (press and hold 
+                the power button for 5 seconds until it flashes and buzzes twice) and click the option
+                that says, "Moxie."
+                </p>
+                <p className="about-text">
+                <h4>TALK DIRTY</h4>
+                When the device connects, a list of commands will open. 
+                You can call any command via voice by clicking the "start" button. Or, click a command card
+                to set your own custom voice command for each action. 
+                </p>
+            </div>
+            <h3>CONTINOUS LISTEN MODE</h3>
+            <p className="about-text">
+                Click "LISTEN MODE" to enter continous listen mode. The App will pick up your commands
+                as you speak so you and your partner can go truly hands free. 
+            </p>
             <button className="about-button" onClick={ back }>BACK</button>
         </section>
     )
